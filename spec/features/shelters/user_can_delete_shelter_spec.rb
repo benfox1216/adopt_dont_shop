9c420a8eec83283143c70574168e99a8f0aea234
Ben Fox <benfox1216@gmail.com>
2020-01-30 08:49:52 -0700

    Add welcome page and delete shelter test

---
+++ spec/features/shelters/user_can_delete_shelter_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "deleting shelter", type: :feature do
+  it "can delete shelter" do
+    shelter_1 = Shelter.create(name: "Your Neighborhood Cats and Dogs",
+                               address: "123 Lake St",
+                               city: "Tulsa",
+                               state: "OK",
+                               zip: "12345")
+    
+    visit "/shelters/#{shelter_1.id}"
+    expect(page).to have_link 'Delete Shelter'
+    
+    click_link("Delete Shelter")
+    expect(page).to have_current_path("/shelters")
+    
+    expect(page).to have_no_content('Your Neighborhood Cats and Dogs')
+    expect(page).to have_no_content('123 Lake St')
+    expect(page).to have_no_content('Tulsa')
+    expect(page).to have_no_content('OK')
+    expect(page).to have_no_content('12345')
+  end
+end
