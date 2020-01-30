4fa7afb04cc5c4f2553fcc5cf139221769bfaee4
Ben Fox <benfox1216@gmail.com>
2020-01-30 04:37:18 -0700

    Return to original state

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  root '/shelters'
+  root 'shelters#index'
   get '/shelters', to: 'shelters#index'
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
