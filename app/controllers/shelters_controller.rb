a50a16ffa6ba8fdc7cdeb4059e6781395251aac1
Ben Fox <benfox1216@gmail.com>
2020-01-30 08:25:15 -0700

    Add passing test & code for updating a shelter

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -3,10 +3,6 @@ class SheltersController < ApplicationController
     @shelters = Shelter.all
   end
   
-  def show
-    @shelter = Shelter.find(params[:id])
-  end
-  
   def new
   end
   
@@ -20,7 +16,28 @@ class SheltersController < ApplicationController
       })
 
     shelter.save
-
     redirect_to '/shelters'
+  end
+  
+  def show
+    @shelter = Shelter.find(params[:id])
+  end
+  
+  def edit
+    @shelter = Shelter.find(params[:id])
+  end
+  
+  def update
+    shelter = Shelter.find(params[:id])
+    shelter.update({
+      name: params[:name],
+      address: params[:address],
+      city: params[:city],
+      state: params[:state],
+      zip: params[:zip]
+      })
+      
+    shelter.save
+    redirect_to "/shelters/#{shelter.id}"
   end
 end
---
+++ spec/features/shelters/user_can_update_shelter_info_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "update shelter", type: :feature do
+  it "can update shelter" do
+    shelter_1 = Shelter.create(name: "Your Neighborhood Cats and Dogs",
+                               address: "123 Lake St",
+                               city: "Tulsa",
+                               state: "OK",
+                               zip: "12345")
+    
+    visit "/shelters/#{shelter_1.id}"
+    expect(page).to have_link 'Update Shelter'
+    
+    click_link("Update Shelter")
+    expect(page).to have_current_path("/shelters/#{shelter_1.id}/edit")
+    
+    expect(page).to have_field 'name'
+    expect(page).to have_field 'address'
+    expect(page).to have_field 'city'
+    expect(page).to have_field 'state'
+    expect(page).to have_field 'zip'
+    expect(page).to have_button 'Update Shelter'
+    
+    fill_in 'name', with: 'Another Shelter'
+    fill_in 'address', with: '456 Pond Ave'
+    fill_in 'city', with: 'West Chester'
+    fill_in 'state', with: 'PA'
+    fill_in 'zip', with: '98765'
+    click_button 'Update Shelter'
+    
+    expect(page).to have_current_path("/shelters/#{shelter_1.id}")
+    expect(page).to have_content('Another Shelter')
+    expect(page).to have_content('456 Pond Ave')
+    expect(page).to have_content('West Chester')
+    expect(page).to have_content('PA')
+    expect(page).to have_content('98765')
+    
+    expect(page).to have_no_content('Your Neighborhood Cats and Dogs')
+    expect(page).to have_no_content('123 Lake St')
+    expect(page).to have_no_content('Tulsa')
+    expect(page).to have_no_content('OK')
+    expect(page).to have_no_content('12345')
+  end
+end
